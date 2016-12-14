4482b2f50a91210f8722ab1e5cd3ead9d94bbc06
Ondrej Sterba <osterba@inbox.com>
2016-12-14 16:50:35 +0100

    Missing licence added

--- SerialPort.c
+++ SerialPort.c
@@ -1,3 +1,19 @@
+/*
+* Windows Serial Port for Windows API
+*
+* Copyright (c) 2016 Ondrej Sterba <osterba@inbox.com>
+*
+* https://github.com/embedded-tools/WindowsSerialPort
+*
+* Permission to use, copy, modify, distribute and sell this software
+* and its documentation for any purpose is hereby granted without fee
+* provided that the above copyright notice appear in all copies and
+* that both that copyright notice and this permission notice appear
+* in supporting documentation.
+* It is provided "as is" without express or implied warranty.
+*
+*/
+
 #include "SerialPort.h"
 #include <stdio.h>
 #include <stdlib.h>
